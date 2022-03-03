e0f7661952b1c362d103f182f32b3608234ba8ac
siddhants1 <siddhant.jag@gmail.com>
2022-03-03 16:52:14 +0530

    Done

--- scripts/script.js
+++ scripts/script.js
@@ -9,12 +9,18 @@ class Validations {
             for (let i = 0; i <= 9; i++) {
                 for (let j = 0; j < this.param.length; j++) {
                     if (this.param[j] === String(i)) {
-                        console.log("Invalid Input");
-                        break;
+                        alert("Invalid Username");
+                        return false;
                     }
                 }
             }
+            return true;
         }
+    }
+
+    validateEmail() {
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return re.test(this.param);
     }
 
 }
@@ -22,12 +28,18 @@ class Validations {
 firstNameSelector = document.querySelector('[placeholder="First Name"]');
 lastNameSelector = document.querySelector('[placeholder="Last Name"]');
 emailSelector = document.querySelector('[placeholder="Email"]');
+submit = document.querySelector(".form-submit-button");
 
 const objectCreator = (selector) => {
-    if(selector.value.length > 0) {
-        return new Validations(selector.value);
-    }
+    return new Validations(selector.value);
 }
 
-// firstNameSelector.addEventListener("input", objectCreator(firstNameSelector).validateName);
-// lastNameSelector.addEventListener("input", objectCreator(lastNameSelector).validateName);
+const submitHandler = () => {
+    if ((objectCreator(firstNameSelector).validateName() === true) && (objectCreator(lastNameSelector).validateName() === true) &&
+     (objectCreator(emailSelector).validateEmail() === true)) {
+        return true;
+    }
+    return false;
+}
+
+submit.addEventListener("click", submitHandler);
